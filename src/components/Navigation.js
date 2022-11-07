aafa457b128c739ee16057ee34e6fdf04a0a226a
gokite227 <102020249+gokite227@users.noreply.github.com>
2022-11-08 07:19:01 +0900

    네비게이션 드롭다운

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,27 +1,34 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "resources/css/style.css"
+import "resources/css/dropdown.css"
 
 const Navigation = () => (
     <div>
-      <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav" style={{backgroundColor:"#d3cbb4"}} >
-  <div class="container px-5">
-      <a class="navbar-brand fw-bold" href="#page-top">Pit a Pet</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-          Menu
-          <i class="bi-list"></i>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarResponsive">
-          <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
-              <li class="nav-item"><Link class="nav-link me-lg-3" to="/">Home</Link></li>
-              <li class="nav-item"><Link class="nav-link me-lg-3" to="/Borad">Borad</Link></li>
-              <li class="nav-item"><Link class="nav-link me-lg-3" to="/CalendarApp">Calendar</Link></li>
-              <li class="nav-item"><a class="nav-link me-lg-3" href="https://gokite227.github.io/kakaomapAPI/">Map</a></li>
-              <li class="nav-item"><Link class="nav-link me-lg-3" to="/profile">profile</Link></li>
-          </ul>
-      </div>
-  </div>
-</nav>    
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top shadow-sm" id="mainNav" style={{ backgroundColor: "#d3cbb4" }} >
+            <div class="container px-5">
+                <div class="dropdown">
+                    <a class="navbar-brand fw-bold" href="#page-top">Pit a Pet</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                        Menu
+                    </button>
+                    <div class="dropdown-content">
+                        <a href="https://gokite227.github.io/pitapet/#/CalendarApp/">Calendar</a>
+                        <a href="https://gokite227.github.io/kakaomapAPI/">Map</a>
+                        <a href="https://gokite227.github.io/pitapet/#/profile/">profile</a>
+                    </div>
+                </div>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto me-4 my-3 my-lg-0">
+                        <li class="nav-item"><Link class="nav-link me-lg-3" to="/">Home</Link></li>
+                        <li class="nav-item"><Link class="nav-link me-lg-3" to="/Borad">Borad</Link></li>
+                        <li class="nav-item"><Link class="nav-link me-lg-3" to="/CalendarApp">Calendar</Link></li>
+                        <li class="nav-item"><a class="nav-link me-lg-3" href="https://gokite227.github.io/kakaomapAPI/">Map</a></li>
+                        <li class="nav-item"><Link class="nav-link me-lg-3" to="/profile">profile</Link></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
     </div>
 );
 export default Navigation;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -96,8 +96,8 @@ const Home= () => {
                                 <h1 class="display-1 lh-1 mb-3">Download the app!</h1>
                                 <p class="lead fw-normal text-muted mb-5">앱을 다운로드하여 반려동물의 심박수와 건강을 기록해 보세요.</p>
                                 <div class="d-flex flex-column flex-lg-row align-items-center">
-                                    <a class="me-lg-3 mb-4 mb-lg-0" href="#!"><img class="app-badge" src={require("resources/assets/img/google-play-badge.svg")} alt="구글" /></a>
-                                    <a href="#!"><img class="app-badge" src={require("resources/assets/img/app-store-badge.svg")} alt="애플" /></a>
+                                    <a class="me-lg-3 mb-4 mb-lg-0" href="#!"><img class="app-badge" src="resources/assets/img/google-play-badge.svg" alt="구글" /></a>
+                                    <a href="#!"><img class="app-badge" src="resources/assets/img/app-store-badge.svg" alt="애플" /></a>
                                 </div>
                             </div>
                         </div>
@@ -145,7 +145,7 @@ const Home= () => {
                 <div class="container px-5">
                     <h2 class="text-center text-white font-alt mb-4">Get the app now!</h2>
                     <div class="d-flex flex-column flex-lg-row align-items-center justify-content-center">
-                        <a class="me-lg-3 mb-4 mb-lg-0" href="#!"><img class="app-badge" src={require("resources/assets/img/google-play-badge.svg")} alt="..." /></a>
+                        <a class="me-lg-3 mb-4 mb-lg-0" href="#!"><img class="app-badge" src="resources/assets/img/google-play-badge.svg" alt="..." /></a>
                         <a href="#!"><img class="app-badge" src="resources/assets/img/app-store-badge.svg" alt="..." /></a>
                     </div>
                 </div>
